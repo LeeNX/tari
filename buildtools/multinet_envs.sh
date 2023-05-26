be828af19c617b72d8552bd0313d1da558090368
C.Lee Taylor <leet@tari.com>
2023-05-26 16:01:08 +0200

    Add changelog per multiNet for libwallet

--- buildtools/multinet_envs.sh
+++ buildtools/multinet_envs.sh
@@ -8,11 +8,13 @@ v*-pre.*)
   echo "esme"
   export TARI_NETWORK=esme
   export TARI_NETWORK_DIR=testnet
+  export TARI_NETWORK_CHANGELOG=development
   ;;
 v*-rc.*)
   echo "nextnet" 
   export TARI_NETWORK=nextnet
   export TARI_NETWORK_DIR=nextnet
+  export TARI_NETWORK_CHANGELOG=nextnet
   ;;
 *)
   echo "mainnet"
